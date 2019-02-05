a6cec8ab1597a77af78e8fe6faf75a292b903c99
almukova1 <43134369+almukova1@users.noreply.github.com>
2019-02-05 19:37:16 +0500

    Update Program.cs

--- labZero/labZero/Program.cs
+++ labZero/labZero/Program.cs
@@ -10,7 +10,7 @@ namespace labZero
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, Regina!");
             Console.ReadLine();
         }
     }
